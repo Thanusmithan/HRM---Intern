93a480e4107374d7ee193c01c78ce5cb02720399
Thanusmithan <114764005+Thanusmithan@users.noreply.github.com>
2024-10-12 14:24:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-// import React from 'react';
-// import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import PayrollForm from "./Pages/PayrollForm";
-// import NavigationBar from './Pages/Navbar';
-// import Dashboard from './Pages/Dashboard'
-// import Employee from './Pages/Employee';
-// import Leave from './Pages/Leave';
-// import Attendance from './Pages/Attendance';
-// import Payroll from './Pages/Payroll';
-// import Administration from './Pages/Administration';
-// import Settings from './Pages/Settings';
-
-// function App() {
-//     return (
-//         <Router>
-//             <NavigationBar />
-//             <div className="container mt-4">
-//                 <Routes>
-//                     <Route path="/" element={<Dashboard />} />
-//                     <Route path="/employee" element={<Employee />} />
-//                     <Route path="/leave" element={<Leave />} />
-//                     <Route path="/attendance" element={<Attendance />} />
-//                     <Route path="/payroll" element={<PayrollForm />} />
-//                     <Route path="/administration" element={<Administration />} />
-//                     <Route path="/settings" element={<Settings />} />
-//                 </Routes>
-//             </div>
-//         </Router>
-//     );
-// }
-
-// export default App;
-
-
-import React from 'react';
+import React from 'react'; 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PayrollForm from "./Pages/PayrollForm";
 import NavigationBar from './Pages/Navbar';
-import Dashboard from './Pages/Dashboard'
+
 import Employee from './Pages/Employee';
 import Leave from './Pages/Leave/Leave';
 import Attendance from './Pages/Attendance';
-import Administration from './Pages/Administration';
-import Settings from './Pages/Settings';
+import Administration from './Pages/Administration'; 
 import Payroll from "./Pages/Payroll";
 import DailyLog from './Pages/DailyLog';
 import TaskManagement from './Pages/Time/TaskManagement';
@@ -63,14 +27,14 @@ function App() {
                     <div className="col-9 col-md-10">
                         <div className="container mt-4">
                             <Routes>
-                                <Route path="/" element={<Dashboard />} />
+                                {/* Redirect root path ("/") to "/employee" */}
+                                <Route path="/" element={<Navigate to="/employee" />} />
                                 <Route path="/employee" element={<Employee />} />
                                 <Route path="/leave" element={<Leave />} />
                                 <Route path="/attendance" element={<Attendance />} />
-                                <Route path="/attendance/daily-log" component={DailyLog} />
+                                <Route path="/attendance/daily-log" element={<DailyLog />} />
                                 <Route path="/payroll" element={<PayrollForm />} />
                                 <Route path="/task" element={<TaskManagement />} />
-                                <Route path="/settings" element={<Settings />} />
                             </Routes>
                         </div>
                     </div>
@@ -81,3 +45,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
